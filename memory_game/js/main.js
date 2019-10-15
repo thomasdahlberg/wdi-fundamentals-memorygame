8921503267af46d19c3c4c13d64baa9be744f055
Thomas Dahlberg <thomasdahlberg@Thomass-MacBook-Pro.local>
2019-10-15 16:07:41 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,18 @@
-const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+function checkForMatch(){
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
 }
+}
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(3);
+flipCard(2);
